7d9507ae525b0ed026d86e74fc608c8b1bd7e9d8
caroline <caroline.program@gmail.com>
2025-02-05 15:25:55 +0100

    Supabase testen

---
+++ pages/test_supabase.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from supabase import create_client, Client
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    url = "https://qnmvdxpiuvtotkkxmgbw.supabase.co"
+    key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFubXZkeHBpdXZ0b3Rra3htZ2J3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwNzk0MDUsImV4cCI6MjA0ODY1NTQwNX0.V27BENxyRWwuNC0YRVcmzL5yhbTO37x_FLDJjdHnWAc"
+    return create_client(url, key)
+
+supabase = init_connection()
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query():
+    return supabase.table("mytable").select("*").execute()
+
+rows = run_query()
+
+# Print results.
+for row in rows.data:
+    st.write(f"{row['name']} has a :{row['pet']}:")
